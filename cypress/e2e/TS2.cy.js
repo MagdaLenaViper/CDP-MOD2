e8ae008124d4f09bd00ed0750ee872592fe190c7
magda <prueba@gmail.com>
2026-02-10 11:18:22 +0100

    TS2

--- cypress/e2e/TS2.cy.js
+++ cypress/e2e/TS2.cy.js
@@ -1,18 +1,33 @@
-describe ('My second scenario', () => {
-    it ('go to Cypress course and make screenshot', function() {
-        cy.visit ('https://fabrykatestow.pl/')
 
-        /*cy.get ('span').contains('POKAŻ CZEGO SIĘ NAUCZĘ')
-        .eq(1) 
-        .click()
-        */
-       cy.contains('span', 'POKAŻ CZEGO SIĘ NAUCZĘ').eq(1).click()
+    describe('My second scenario', () => {
+  it('go to Cypress course and make screenshot', function() {
+    cy.visit('https://fabrykatestow.pl/')
 
-        cy.url().should('eq', 'https://fabrykatestow.pl/cypress/')
-    })
-    })
 
-    // <ul id="nv-primary-navigation-main" class="primary-menu-ul nav-ul menu-desktop"><li id="menu-item-923" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-923"><div class="wrap"><a href="https://fabrykatestow.pl/ciekawostki/">Newsletter</a></div></li>
-// <span class="elementor-button-text">POKAŻ CZEGO SIĘ NAUCZĘ</span>
-//<a class="elementor-button elementor-button-link elementor-size-sm elementor-animation-grow" href="https://fabrykatestow.pl/cypress">
+    cy.get('a[href*="cypress"]')
+      .contains('POKAŻ CZEGO SIĘ NAUCZĘ')
+      .click()
+    cy.url().should('eq', 'https://fabrykatestow.pl/cypress/')
+
+    /* ? prze'scroll'ować się do sekcji "KTO NAUCZY CIĘ TESTÓW AUTOMATYCZNYCH?" */
+    cy.contains('.elementor-heading-title', 'Kto nauczy Cię testów automatycznych?')
+    .closest('.elementor-section')
+    .scrollIntoView({ block: 'center' })
+    .should('be.visible')
+    cy.wait(1000)
+    cy.contains('.elementor-heading-title', 'Kto nauczy Cię testów automatycznych?')
+    .closest('.elementor-section')
+    .screenshot('kto-nauczy-cie-testow') // Zapisz screenshot z nazwą "kto-nauczy-cie-testow-automatycznych.png" w folderze "cypress/screenshots"
+
+    
+    /* ? zrobić screenshot */
+   // cy.screenshot()
+
+    /* ? zapisać ten screenshot na dysku */
+
+
+  })
+})
+
+
 						
